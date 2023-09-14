a83c69568e3a6f0f9e6c8a2d6c0011200ab58b80
IsaacLeen lee <Isaacharrisonlee@gmail.com>
2023-09-14 21:31:28 +0000

    Made glorified rock,paper,scissors

--- period6.py
+++ period6.py
@@ -1,20 +1,37 @@
 import random
-options = ["persuade", "fight", "scream", "sleep"]
+options = ["block", "punch", "takedown", "sleep"]
 
 npc_choice = random.choice(options)
 
-player_choice = input("Do you wanna run, hide, fight, sleep")
+player_choice = input("Do you wanna block, punch, takedown, or sleep")
 
-def pvp():
-    if player_choice == npc_choice:
+print("The npc picked ", npc_choice)
+def pvp(npc_choice, player_choice):
+    if player_choice == "block" and npc_choice == "block" or player_choice == "punch" and npc_choice == "punch" or player_choice == "takedown" and npc_choice == "takedown":
         print("NEGAHOW did you pick the same option")
-    if player_choice == "sleep":
+    if player_choice and npc_choice == "sleep":
+        print("nap time, mimimimimi")
+    elif player_choice == "sleep":
         print("NEGAWATT. You died")
-    if player_choice == "persuade":
-        if npc_choice == "fight":
-            print("You persuade him not to beat you up")
-        if npc_choice == "scream":
-            print("You cant persuade a screamer")
-    
+    elif npc_choice == "sleep":
+        print("Not worth my trouble pipsqueak")
 
-pvp()
+
+    if player_choice == "block":
+        if npc_choice == "punch":
+            print("You blocked and countered, you win!")
+        if npc_choice == "takedown":
+            print("You are a monke and got takendown, you lost!")
+
+    if player_choice == "punch":
+        if npc_choice == "takedown":
+            print("You punched and knocked them out, you win!")
+        if npc_choice == "block":
+            print("You punched and got countered, you lost!")
+
+    if player_choice == "takedown":
+        if npc_choice == "block":
+            print("You took him down, you win!")
+        if npc_choice == "punch":
+            print("You got knocked out, better luck next time")
+pvp(npc_choice, player_choice)
